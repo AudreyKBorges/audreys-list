17f79cbd5e0f6fb938618c18edbd24977827ee26
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-01-31 15:16:34 -0600

    Edits to theme

--- wp-content/themes/audreyslist/footer.php
+++ wp-content/themes/audreyslist/footer.php
@@ -1,9 +1,15 @@
 <?php 
 /**
- * Description: This is a custom theme created for audreyslist.org
- * 
- * @package Theme_AudreysList
+ * Theme audreysList
  * 
  * @since Twenty Twentytwo 1.0
  */
 ?>
+
+    <footer>
+        Copyright &copy; 2019
+        <script>new Date().getFullYear() > 2019 && document.write("-" + new Date().getFullYear());</script>,
+        Audrey's List.
+    </footer>
+  </body>
+</html>
--- wp-content/themes/audreyslist/header.php
+++ wp-content/themes/audreyslist/header.php
@@ -1,24 +1,32 @@
 <?php 
 /**
- * Description: This is a custom theme created for audreyslist.org
+ * Theme audreysList
  * 
- * @package Theme_AudreysList
- * @author Audrey Borges audreyborges7@gmail.com
  * @since Twenty Twentytwo 1.0
- * 
- * PHP 7.4.27
  */
 ?>
 
-<div id="header">
- <div id="headerimg">
-   <h1>
-    <a href="<?php echo get_option('home'); ?>">
-       <?php bloginfo('name'); ?></a>
-   </h1>
-     <div class="description">
-       <?php bloginfo('description'); ?>
-     </div>
-     
-  </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Audrey's List</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet">
+  </head>
+  <header>
+    <a href="index.html">
+    <img id="logo" src="logo.png" class="logo" alt="Audrey's List">
+    </a>
+    <p class="header-text">Keep in touch.</p>
+    <p class="header-text2">info@audreyslist.org.</p>
+    <img class="fb-icon" src="facebook.png" alt="facebook icon">
+    <img class="instagram-icon" src="instagram.png" alt="Instagram icon">
+    <img class="twitter-icon" src="twitter.png" alt="twitter icon">
+  </header>
